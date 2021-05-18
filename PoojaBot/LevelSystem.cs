96f51aa0c0203efd15cb41317e0f1fb3677e3aa0
Quentin <50674795+BBonless@users.noreply.github.com>
2021-05-19 00:53:30 +0200

    Add files via upload

---
+++ PoojaBot/LevelSystem.cs
@@ -0,0 +1,132 @@
+﻿using DSharpPlus.Entities;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoojaBot
+{ 
+
+    class LevelSystem
+    {
+        private static PlayerData PlayerData;
+
+        private static string DataPath;
+
+        public LevelSystem(string DataPathIn)
+        {
+            PlayerData = new PlayerData(File.ReadAllLines(DataPathIn));
+
+            DataPath = DataPathIn;
+        }
+
+        public void Register(ulong ID)
+        {
+            PlayerData.Data.Add(ID, new Data(0, 0, DateTimeOffset.FromUnixTimeSeconds(0)));
+        }
+
+        public void AdminAdd(ulong ID, int Amount, string Type)
+        {
+            if ( !PlayerData.Data.ContainsKey(ID) )
+            {
+                Register(ID);
+            }
+
+            switch ( Type )
+            {
+                case "levels":
+                    PlayerData.Data[ID].Level += Amount;
+                    break;
+                case "points":
+                    PlayerData.Data[ID].XP += Amount;
+                    break;
+                case "xp":
+                    PlayerData.Data[ID].XP += Amount;
+                    break;
+                default:
+                    break;
+            }
+
+            if ( PlayerData.Data[ID].XP >= Math.Pow(PlayerData.Data[ID].Level - 1, 2) * 10 )
+            {
+                LevelUp(ID);
+            }
+
+        }
+
+        public void NaturalAdd(ulong ID)
+        {
+            if ( !PlayerData.Data.ContainsKey(ID) )
+            {
+                Register(ID);
+            }
+
+            DateTimeOffset Now = DateTimeOffset.Now;
+
+            double SecondsSinceLastMessage = Now.Subtract(PlayerData.Data[ID].LastMessageTime).TotalSeconds;
+
+            PlayerData.Data[ID].LastMessageTime = Now;
+
+            if (SecondsSinceLastMessage < 2)
+            {
+                Util.Log("Did not grant XP for message to <@", ID, "> due to Spam Protection Timer");
+                return;
+            }
+
+            if (++PlayerData.Data[ID].XP >= Math.Pow(PlayerData.Data[ID].Level - 1, 2) * 10)
+            {
+                LevelUp(ID);
+            }
+
+            PlayerData.Save(DataPath);
+            Util.Log("Granted XP to <@", ID, ">");
+        }
+
+        public void QueryStatus(DiscordMessage MSG)
+        {
+            ulong ID = MSG.Author.Id;
+
+            if (!PlayerData.Data.ContainsKey(ID))
+            {
+                Register(ID);
+            }
+
+            int LV = PlayerData.Data[ID].Level;
+            int XP = PlayerData.Data[ID].XP;
+
+            MSG.Channel.SendMessageAsync(string.Format("<@{0}>, Your current level is {1}, and you have {2} xp!", ID, LV, XP));
+        }
+
+        public void QueryToNext(DiscordMessage MSG)
+        {
+            ulong ID = MSG.Author.Id;
+
+            int Max = (int)Math.Pow(PlayerData.Data[ID].Level - 1, 2) * 10;
+
+            int Current = PlayerData.Data[ID].XP;
+
+            if ( !PlayerData.Data.ContainsKey(ID) )
+            {
+                Register(ID);
+            }
+
+            MSG.Channel.SendMessageAsync(string.Format("<@{0}>, You need {1} xp to level up! ({2}/{3})", ID, Max - Current, Current, Max));
+        }
+
+        private void LevelUp(ulong ID)
+        {
+            PlayerData.Data[ID].Level++;
+            PlayerData.Data[ID].XP = 0;
+
+            Mainframe.Announce("<@", ID, ">", " Has levelled up to level ", PlayerData.Data[ID].Level, "!!");
+        }
+    }
+
+    //Query, return the Level of a User
+    //AdminAdd, Add N Levels to a User
+    //AdminRemove, Remove N Levels from a User
+    //Add, Qualify and Add Natural XP to a user
+}
---
+++ PoojaBot/Mainframe.cs
@@ -0,0 +1,137 @@
+﻿using DSharpPlus;
+using DSharpPlus.Entities;
+using DSharpPlus.EventArgs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PoojaBot
+{
+    class Mainframe
+    {
+        const long BOTID = 843883099116142622;
+
+        static ulong AnnouncementChannelID = 0;
+
+        static LevelSystem Levels;
+
+        static DiscordClient Discord;
+
+        static void Main( string[] args )
+        {
+            Levels = new LevelSystem("C:\\Users\\quent\\Desktop\\POOJAPLAYERDATA.txt");
+
+            StartBot().GetAwaiter().GetResult();
+        }
+
+        static async Task StartBot()
+        {
+            Discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = "ODQzODgzMDk5MTE2MTQyNjIy.YKKVYQ.AQvT6W5ApOlh3PVSxolwOZWMZP4",
+                TokenType = TokenType.Bot
+            });
+
+            Discord.MessageCreated += async (Client, MSG) =>
+            {
+                Task.Run(() => HandleMessageCreated(MSG));
+            };
+
+            await Discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        static async Task HandleMessageCreated(MessageCreateEventArgs MSG)
+        {
+            string Content = MSG.Message.Content;
+
+            if (MSG.Author.Id == BOTID)
+            {
+                MSG.Handled = true; return;
+            }
+
+            if (!(Content.StartsWith("Pooja,") || Content.StartsWith("P,")))
+            {
+                HandleNoCommand(MSG.Message);
+
+                MSG.Handled = true; return;
+            }
+
+            HandleCommand(MSG.Message);
+
+            MSG.Handled = true; return;
+        }
+
+
+        static async Task HandleNoCommand(DiscordMessage MSG)
+        {
+            Levels.NaturalAdd(MSG.Author.Id);
+        }
+
+        static async Task HandleCommand(DiscordMessage MSG)
+        {
+            string[] Command = Util.ExtractCommand(MSG.Content.ToLower()).Split(' ');
+
+            switch ( Command[0] )
+            {
+                case "rank":
+                    Levels.QueryStatus(MSG);
+                    break;
+                case "next":
+                    Levels.QueryToNext(MSG);
+                    break;
+                case "add":
+                    // 1 = Amount
+                    // 2 = Type
+                    // 3 = To (OPT)
+                    // 4 = User
+                    
+                    if (Command.Length <= 2)
+                    {
+                        Levels.AdminAdd(Util.GetIDFromTag(Command[4]), int.Parse(Command[1]), Command[2]);
+                    }
+                    else
+                    {
+                        Levels.AdminAdd(MSG.Author.Id, int.Parse(Command[1]), Command[2]);
+                    }
+
+                    Util.SendMessageThenDelete(MSG.Channel, "Done!", 1000);
+                    break;
+                case "set":
+                    switch ( Command[1] )
+                    {
+                        case "announcements":
+                            AnnouncementChannelID = MSG.ChannelId;
+                            Util.SendMessageThenDelete(MSG.Channel, "Set Announcment channel to current channel!", 1000);
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public static async Task Announce(params object[] O)
+        {
+            StringBuilder SB = new StringBuilder();
+
+            foreach ( object O1 in O )
+            {
+                SB.Append(O1);
+            }
+
+            if ( AnnouncementChannelID == 0 )
+            {
+                Util.Log("Tried to announce the following: '", SB.ToString(), "' but no announcement channel was set :(");
+                return;
+            }
+
+            (await Discord.GetChannelAsync(AnnouncementChannelID)).SendMessageAsync(SB.ToString());
+        }
+    }
+}
